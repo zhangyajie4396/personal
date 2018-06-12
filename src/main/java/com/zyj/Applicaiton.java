e2caa5b541f9e9485a48ca7d9ef05ce789f2a46d
zhangyajie <729834586@qq.com>
2018-06-12 22:27:07 +0800

    test

--- src/main/java/com/zyj/Applicaiton.java
+++ src/main/java/com/zyj/Applicaiton.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Applicaiton {
 
     public static void main(String[] args) {
+        System.out.println(1111);
         SpringApplication.run(Applicaiton.class, args);
     }
 }
